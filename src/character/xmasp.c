c3ac633ba839c11c6ecc9d4c5a4f8c806e5c440d
UNSTOP4BLE <bruno.juraski1@gmail.com>
2021-11-16 23:43:32 +0100

    funni mouse
    added sad mouse

--- src/character/xmasp.c
+++ src/character/xmasp.c
@@ -1,3 +1,9 @@
+/*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #include "xmasp.h"
 
 #include "../mem.h"
@@ -12,22 +18,26 @@ enum
 	XmasP_ArcMain_Idle1,
 	XmasP_ArcMain_Idle2,
 	XmasP_ArcMain_Idle3,
+	XmasP_ArcMain_Idle4,
+	XmasP_ArcMain_Idle5,
+	XmasP_ArcMain_Idle6,
+	XmasP_ArcMain_Idle7,
 	XmasP_ArcMain_LeftA0,
 	XmasP_ArcMain_LeftA1,
-	XmasP_ArcMain_LeftB0,
-	XmasP_ArcMain_LeftB1,
+	XmasP_ArcMain_LeftA2,
+	XmasP_ArcMain_LeftA3,
 	XmasP_ArcMain_DownA0,
 	XmasP_ArcMain_DownA1,
-	XmasP_ArcMain_DownB0,
-	XmasP_ArcMain_DownB1,
+	XmasP_ArcMain_DownA2,
+	XmasP_ArcMain_DownA3,
 	XmasP_ArcMain_UpA0,
 	XmasP_ArcMain_UpA1,
-	XmasP_ArcMain_UpB0,
-	XmasP_ArcMain_UpB1,
+	XmasP_ArcMain_UpA2,
+	XmasP_ArcMain_UpA3,
 	XmasP_ArcMain_RightA0,
 	XmasP_ArcMain_RightA1,
-	XmasP_ArcMain_RightB0,
-	XmasP_ArcMain_RightB1,
+	XmasP_ArcMain_RightA2,
+	XmasP_ArcMain_RightA3,
 	
 	XmasP_Arc_Max,
 };
@@ -47,42 +57,46 @@ typedef struct
 
 //Christmas Parents definitions
 static const CharFrame char_xmasp_frame[] = {
-	{XmasP_ArcMain_Idle0, {0,   0, 127, 206}, { 60, 203}}, //0 idle 1
-	{XmasP_ArcMain_Idle1, {0,   0, 127, 205}, { 58, 202}}, //1 idle 2
-	{XmasP_ArcMain_Idle2, {0,   0, 127, 205}, { 58, 202}}, //2 idle 3
-	{XmasP_ArcMain_Idle3, {0,   0, 127, 206}, { 60, 203}}, //3 idle 4
+	{XmasP_ArcMain_Idle0, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_Idle1, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_Idle2, {0,   0, 212, 200}, {130, 182}},  
+	{XmasP_ArcMain_Idle3, {0,   0, 212, 200}, {130, 182}},  
+	{XmasP_ArcMain_Idle4, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_Idle5, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_Idle6, {0,   0, 212, 200}, {130, 182}},  
+	{XmasP_ArcMain_Idle7, {0,   0, 212, 200}, {130, 182}},  
+
+	{XmasP_ArcMain_LeftA0, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_LeftA1, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_LeftA2, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_LeftA3, {0,   0, 212, 200}, {130, 182}}, 
+
+	{XmasP_ArcMain_DownA0, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_DownA1, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_DownA2, {0,   0, 212, 200}, {130, 182}},  
+	{XmasP_ArcMain_DownA3, {0,   0, 212, 200}, {130, 182}}, 
 	
-	{XmasP_ArcMain_LeftA0, {0,   0, 135, 195}, {108, 193}}, //4 left a 1
-	{XmasP_ArcMain_LeftA1, {0,   0, 147, 194}, {115, 193}}, //5 left a 2
-	{XmasP_ArcMain_LeftB0, {0,   0, 147, 212}, { 52, 188}}, //6 left b 1
-	{XmasP_ArcMain_LeftB1, {0,   0, 143, 207}, { 55, 188}}, //7 left b 2
+	{XmasP_ArcMain_UpA0, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_UpA1, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_UpA2, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_UpA3, {0,   0, 212, 200}, {130, 182}}, 
 	
-	{XmasP_ArcMain_DownA0, {0,   0, 126, 216}, { 95, 200}}, //8 down a 1
-	{XmasP_ArcMain_DownA1, {0,   0, 127, 207}, { 95, 192}}, //9 down a 2
-	{XmasP_ArcMain_DownB0, {0,   0, 147, 212}, { 80, 195}}, //10 down b 1
-	{XmasP_ArcMain_DownB1, {0,   0, 143, 207}, { 80, 192}}, //11 down b 2
-	
-	{XmasP_ArcMain_UpA0, {0,   0, 159, 227}, { 58, 213}}, //12 up a 1
-	{XmasP_ArcMain_UpA1, {0,   0, 159, 233}, { 58, 218}}, //13 up a 2
-	{XmasP_ArcMain_UpB0, {0,   0, 147, 212}, { 47, 211}}, //14 up b 1
-	{XmasP_ArcMain_UpB1, {0,   0, 143, 207}, { 49, 210}}, //15 up b 2
-	
-	{XmasP_ArcMain_RightA0, {0,   0, 145, 216}, { 57, 214}}, //16 right a 1
-	{XmasP_ArcMain_RightA1, {0,   0, 137, 220}, { 63, 217}}, //17 right a 2
-	{XmasP_ArcMain_RightB0, {0,   0, 147, 212}, { 78, 191}}, //18 right b 1
-	{XmasP_ArcMain_RightB1, {0,   0, 143, 207}, { 60, 198}}, //19 right b 2
+	{XmasP_ArcMain_RightA0, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_RightA1, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_RightA2, {0,   0, 212, 200}, {130, 182}}, 
+	{XmasP_ArcMain_RightA3, {0,   0, 212, 200}, {130, 182}}, 
 };
 
 static const Animation char_xmasp_anim[CharAnim_Max] = {
-	{2, (const u8[]){ 1,  2,  3,  0, ASCR_BACK, 1}}, //CharAnim_Idle
-	{2, (const u8[]){ 4,  5, ASCR_BACK, 1}},         //CharAnim_Left
-	{2, (const u8[]){ 6,  7, ASCR_BACK, 1}},         //CharAnim_LeftAlt
-	{2, (const u8[]){ 9,  8, ASCR_BACK, 1}},         //CharAnim_Down
-	{2, (const u8[]){10, 11, ASCR_BACK, 1}},         //CharAnim_DownAlt
-	{2, (const u8[]){13, 12, ASCR_BACK, 1}},         //CharAnim_Up
-	{2, (const u8[]){14, 15, ASCR_BACK, 1}},         //CharAnim_UpAlt
-	{2, (const u8[]){16, 17, ASCR_BACK, 1}},         //CharAnim_Right
-	{2, (const u8[]){18, 19, ASCR_BACK, 1}},         //CharAnim_RightAlt
+	{2, (const u8[]){ 0, 1, 2, 3, 4, 5, 6, 7, ASCR_BACK, 1}}, //CharAnim_Idle
+	{2, (const u8[]){ 8, 9, 10, 11, ASCR_BACK, 1}},         //CharAnim_Left
+	{0, (const u8[]){ASCR_CHGANI, CharAnim_Idle}},
+	{2, (const u8[]){ 12, 13, 14, 15, ASCR_BACK, 1}},         //CharAnim_Down
+	{0, (const u8[]){ASCR_CHGANI, CharAnim_Idle}},
+	{2, (const u8[]){ 16, 17, 18, 19, ASCR_BACK, 1}},         //CharAnim_Up
+	{0, (const u8[]){ASCR_CHGANI, CharAnim_Idle}},
+	{2, (const u8[]){ 20, 21, 22, 23, ASCR_BACK, 1}},         //CharAnim_Right
+	{0, (const u8[]){ASCR_CHGANI, CharAnim_Idle}},
 };
 
 //Christmas Parents functions
@@ -104,20 +118,6 @@ void Char_XmasP_Tick(Character *character)
 {
 	Char_XmasP *this = (Char_XmasP*)character;
 	
-	if ((stage.stage_id == StageId_1_1) && stage.song_step >= 764)
-	{
-		this->character.focus_x = FIXED_DEC(25, 1);
-		this->character.focus_y = FIXED_DEC(-100, 1);
-		this->character.focus_zoom = FIXED_DEC(13, 10);
-	}
-
-	if ((stage.stage_id == StageId_1_1) && stage.song_step >= 784)
-	{
-		this->character.focus_x = FIXED_DEC(25, 1);
-		this->character.focus_y = FIXED_DEC(-100, 1);
-		this->character.focus_zoom = FIXED_DEC(1, 1);
-	}
-
 	//Perform idle dance
 	if ((character->pad_held & (INPUT_LEFT | INPUT_DOWN | INPUT_UP | INPUT_RIGHT)) == 0)
 		Character_PerformIdle(character);
@@ -161,7 +161,7 @@ Character *Char_XmasP_New(fixed_t x, fixed_t y)
 	Animatable_Init(&this->character.animatable, char_xmasp_anim);
 	Character_Init((Character*)this, x, y);
 	
-	//Set character stage information
+	//Set character information
 	this->character.spec = 0;
 	
 	this->character.health_i = 5;
@@ -178,22 +178,26 @@ Character *Char_XmasP_New(fixed_t x, fixed_t y)
 		"idle1.tim",   //XmasP_ArcMain_Idle1
 		"idle2.tim",   //XmasP_ArcMain_Idle2
 		"idle3.tim",   //XmasP_ArcMain_Idle3
+		"idle4.tim",   //XmasP_ArcMain_Idle0
+		"idle5.tim",   //XmasP_ArcMain_Idle1
+		"idle6.tim",   //XmasP_ArcMain_Idle2
+		"idle7.tim",   //XmasP_ArcMain_Idle3
 		"lefta0.tim",  //XmasP_ArcMain_LeftA0
 		"lefta1.tim",  //XmasP_ArcMain_LeftA1
-		"leftb0.tim",  //XmasP_ArcMain_LeftB0
-		"leftb1.tim",  //XmasP_ArcMain_LeftB1
+		"lefta2.tim",  //XmasP_ArcMain_LeftB0
+		"lefta3.tim",  //XmasP_ArcMain_LeftB1
 		"downa0.tim",  //XmasP_ArcMain_DownA0
 		"downa1.tim",  //XmasP_ArcMain_DownA1
-		"downb0.tim",  //XmasP_ArcMain_DownB0
-		"downb1.tim",  //XmasP_ArcMain_DownB1
+		"downa2.tim",  //XmasP_ArcMain_DownB0
+		"downa3.tim",  //XmasP_ArcMain_DownB1
 		"upa0.tim",    //XmasP_ArcMain_UpA0
 		"upa1.tim",    //XmasP_ArcMain_UpA1
-		"upb0.tim",    //XmasP_ArcMain_UpB0
-		"upb1.tim",    //XmasP_ArcMain_UpB1
+		"upa2.tim",    //XmasP_ArcMain_UpB0
+		"upa3.tim",    //XmasP_ArcMain_UpB1
 		"righta0.tim", //XmasP_ArcMain_RightA0
 		"righta1.tim", //XmasP_ArcMain_RightA1
-		"rightb0.tim", //XmasP_ArcMain_RightB0
-		"rightb1.tim", //XmasP_ArcMain_RightB1
+		"righta2.tim", //XmasP_ArcMain_RightB0
+		"righta3.tim", //XmasP_ArcMain_RightB1
 		NULL
 	};
 	IO_Data *arc_ptr = this->arc_ptr;
